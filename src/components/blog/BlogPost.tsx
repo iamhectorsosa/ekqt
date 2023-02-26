e67d2bb1cf904c7fcb4e1d3729ca79283220bee1
Hector Sosa <hello@hectorsosa.me>
2023-02-26 10:48:02 +0100

    Enable Scroll-linked animation for Blog post

--- src/components/blog/BlogPost.tsx
+++ src/components/blog/BlogPost.tsx
@@ -5,6 +5,7 @@ import { MDXRemote, type MDXRemoteSerializeResult } from "next-mdx-remote";
 import { Settings } from "@/sanity/schemas/settings";
 import { mdxComponents } from "@/lib/mdxComponents";
 import { ProseH1, ProseSubtle } from "../ui/typography";
+import { motion, useScroll, useSpring } from "framer-motion";
 
 export default function BlogPost({
   post,
@@ -17,6 +18,13 @@ export default function BlogPost({
   socials: Settings["socials"];
   preview?: boolean;
 }) {
+  const { scrollYProgress } = useScroll();
+  const scaleX = useSpring(scrollYProgress, {
+    stiffness: 100,
+    damping: 30,
+    restDelta: 0.001,
+  });
+
   return (
     <Layout
       title={post.title}
@@ -25,27 +33,35 @@ export default function BlogPost({
       socials={socials}
       preview={preview}
     >
-      <section>
-        <header className="mb-6 space-y-2">
-          <ProseH1>{post.title}</ProseH1>
-          <ProseSubtle>
-            {!post.publishedAt || !post.readingTime ? (
-              "Draft"
-            ) : (
-              <>
-                <span>
-                  {dayjs(post.publishedAt).format("dddd DD MMMM YYYY")}
-                </span>
-                {" · "}
-                <span> {post.readingTime} min read</span>
-              </>
-            )}
-          </ProseSubtle>
-        </header>
-        <article aria-label="Post">
-          <MDXRemote {...source} components={mdxComponents} />
-        </article>
-      </section>
+      <>
+        <motion.div
+          className={`fixed inset-0 z-50 h-2 origin-left bg-slate-700 dark:bg-slate-300 ${
+            preview ? "top-11" : ""
+          }`}
+          style={{ scaleX }}
+        />
+        <section>
+          <header className="mb-6 space-y-2">
+            <ProseH1>{post.title}</ProseH1>
+            <ProseSubtle>
+              {!post.publishedAt || !post.readingTime ? (
+                "Draft"
+              ) : (
+                <>
+                  <span>
+                    {dayjs(post.publishedAt).format("dddd DD MMMM YYYY")}
+                  </span>
+                  {" · "}
+                  <span> {post.readingTime} min read</span>
+                </>
+              )}
+            </ProseSubtle>
+          </header>
+          <article aria-label="Post">
+            <MDXRemote {...source} components={mdxComponents} />
+          </article>
+        </section>
+      </>
     </Layout>
   );
 }
